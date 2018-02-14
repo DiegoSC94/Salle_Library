752ba9f58f62fdb24a1dc300e2695fc19f5218fa
rubenniki <rubenniki@gmail.com>
2018-02-14 19:57:37 +0100

    splash and manifest

--- app/src/main/java/rubendiego/salle_library/SplashMainActivity.java
+++ app/src/main/java/rubendiego/salle_library/SplashMainActivity.java
@@ -3,11 +3,36 @@ package rubendiego.salle_library;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.view.Window;
+
 public class SplashMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_splash_main);
+getSupportActionBar().hide();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                // Start the next activity
+                Intent mainIntent = new Intent().setClass(
+                        SplashMainActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+
+
+                finish();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, 3000);
+
     }
 }
