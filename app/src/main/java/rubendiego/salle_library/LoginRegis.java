9275d5f7dbb8a0b0c2eb1596cdef85acbf533dcf
rubenniki <rubenniki@gmail.com>
2018-02-14 20:16:54 +0100

    login y register

--- app/src/main/java/rubendiego/salle_library/LoginRegis.java
+++ app/src/main/java/rubendiego/salle_library/LoginRegis.java
@@ -1,5 +1,7 @@
 package rubendiego.salle_library;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,12 @@ public class LoginRegis extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_regis);
+        SharedPreferences prefs =
+                getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("email", "modificado@email.com");
+        editor.putString("nombre", "Prueba");
+        editor.commit();
     }
 }
