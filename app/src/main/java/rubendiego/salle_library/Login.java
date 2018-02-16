f09c0d73326afe779c8cfa7a32291142d46ad6a9
rubenniki <rubenniki@gmail.com>
2018-02-16 01:05:19 +0100

    link i login 1/2

--- app/src/main/java/rubendiego/salle_library/Login.java
+++ app/src/main/java/rubendiego/salle_library/Login.java
@@ -1,18 +1,22 @@
 package rubendiego.salle_library;
 
 
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Login extends Fragment {
+public class Login extends Fragment implements View.OnClickListener {
 
+    Button login,register;
 
     public Login() {
         // Required empty public constructor
@@ -22,8 +26,31 @@ public class Login extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view=inflater.inflate(R.layout.fragment_login, container, false);
+        login.findViewById(R.id.iniciar);
+        register.findViewById(R.id.register);
+
+        login.setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.register:
+                Fragment register = new Register();
+
+                FragmentTransaction transation = getActivity().getFragmentManager().beginTransaction();
+
+                transation.replace(R.id.fragment_register, register);
+                transation.addToBackStack(null);
+                transation.commit();
+                break;
+            case R.id.iniciar:
+                Intent intent = new Intent(getActivity(),MainPage.class);
+                getActivity().startActivity(intent);
+                break;
+        }
+    }
 }
--- app/src/main/java/rubendiego/salle_library/Register.java
+++ app/src/main/java/rubendiego/salle_library/Register.java
@@ -4,7 +4,7 @@ package rubendiego.salle_library;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
