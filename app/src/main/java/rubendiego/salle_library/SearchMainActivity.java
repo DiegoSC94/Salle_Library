182b23391f6a8d54f14576bc83ae0a9baf9adb2a
DiegoSC94 <diego.santamarta@gracia.lasalle.cat>
2018-02-18 19:49:04 +0100

    xml search

---
+++ app/src/main/java/rubendiego/salle_library/SearchMainActivity.java
@@ -0,0 +1,23 @@
+package rubendiego.salle_library;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class SearchMainActivity extends AppCompatActivity {
+
+    private EditText tituloLibro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_main);
+
+        tituloLibro = findViewById(R.id.titulo_libro);
+    }
+
+    public void buscarLibro(View v){
+
+    }
+}
