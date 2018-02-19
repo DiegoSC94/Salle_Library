e1d5684346dfc8478ec1b160696cef31f0e215a7
rubenniki <rubenniki@gmail.com>
2018-02-19 11:41:29 +0100

    arreglos action bar

--- app/src/main/java/rubendiego/salle_library/MainPage.java
+++ app/src/main/java/rubendiego/salle_library/MainPage.java
@@ -5,19 +5,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
-import java.sql.PreparedStatement;
 
-public class MainPage extends Activity implements View.OnClickListener {
+public class MainPage extends AppCompatActivity implements View.OnClickListener {
 
     private Button logOff;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
         logOff = findViewById(R.id.logOff);
         logOff.setOnClickListener(this);
     }
