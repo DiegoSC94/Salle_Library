ed7b7030d5211e7eb909d86c3e60a58d2f285f63
rubenniki <rubenniki@gmail.com>
2018-03-16 00:58:22 +0100

    faborito

--- app/src/main/java/rubendiego/salle_library/OpenBook.java
+++ app/src/main/java/rubendiego/salle_library/OpenBook.java
@@ -12,6 +12,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -22,8 +23,7 @@ import com.squareup.picasso.Picasso;
 public class OpenBook extends AppCompatActivity{
 public TextView titulo,autor,descripcion;
 public ImageView imageView;
-   public Intent intentLibro=getIntent();
-public  Book Libro=intentLibro.getParcelableExtra("Libro");
+public  Book Libro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +33,8 @@ public  Book Libro=intentLibro.getParcelableExtra("Libro");
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-
+        Intent intentLibro=getIntent();
+        Libro=intentLibro.getParcelableExtra("Libro");
         titulo=findViewById(R.id.titulo_Open);
         autor=findViewById(R.id.autores_Open);
         descripcion=findViewById(R.id.descripcion_Open);
@@ -58,13 +59,15 @@ public  Book Libro=intentLibro.getParcelableExtra("Libro");
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.addBar:
+                /*
                 SharedPreferences datos = getSharedPreferences("faboritos", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = datos.edit();
                 editor.putString("Libro", String.valueOf(Libro));
 
                 editor.commit();
                 Intent intent = new Intent(this, LoginRegis.class);
-                startActivity(intent);
+                startActivity(intent);*/
+                Toast.makeText(this,"Añadido a faboritos",Toast.LENGTH_LONG).show();
                 break;
             default:
                 return super.onOptionsItemSelected(item);
